4652941e6d602ce5390b301283b8d221849aac6d
aleksandr.chukanov <aleksandr.chukanov@krit.pro>
2025-03-25 16:31:49 +0300

    - done, tested

--- CoderbyteTest_SudokuQuadrantChecker/Program.cs
+++ CoderbyteTest_SudokuQuadrantChecker/Program.cs
@@ -6,12 +6,53 @@ using System.Threading.Tasks;
 
 namespace CoderbyteTest_SudokuQuadrantChecker
 {
-    internal class Program
+    public class Program
     {
         static int dimX = 9;
         static int dimY = 9;
+        /*
+        static string[] strArr = 
+        {   
+            "(1,2,3,4,5,6,7,8,1)", 
+            "(x,x,x,x,x,x,x,x,x)", 
+            "(x,x,x,x,x,x,x,x,x)", 
+            "(1,x,x,x,x,x,x,x,x)", 
+            "(x,x,x,x,x,x,x,x,x)", 
+            "(x,x,x,x,x,x,x,x,x)", 
+            "(x,x,x,x,x,x,x,x,x)", 
+            "(x,x,x,x,x,x,x,x,x)", 
+            "(x,x,x,x,x,x,x,x,x)" 
+        };
+        */
 
-        static string[] strArr = { "(1,2,3,4,5,6,7,8,1)", "(x,x,x,x,x,x,x,x,x)", "(x,x,x,x,x,x,x,x,x)", "(1,x,x,x,x,x,x,x,x)", "(x,x,x,x,x,x,x,x,x)", "(x,x,x,x,x,x,x,x,x)", "(x,x,x,x,x,x,x,x,x)", "(x,x,x,x,x,x,x,x,x)", "(x,x,x,x,x,x,x,x,x)" };
+        /*
+        static string[] strArr =
+        {
+            "(1,2,3,4,5,6,7,8,9)",
+            "(x,x,x,x,x,x,x,x,x)",
+            "(6,x,5,x,3,x,x,4,x)",
+            "(2,x,1,1,x,x,x,x,x)",
+            "(x,x,x,x,x,x,x,x,x)",
+            "(x,x,x,x,x,x,x,x,x)",
+            "(x,x,x,x,x,x,x,x,x)",
+            "(x,x,x,x,x,x,x,x,x)",
+            "(x,x,x,x,x,x,x,x,9)"
+        };
+        */
+
+       static string[] strArr =
+       {
+            "(1,2,3,4,5,6,7,8,1)",
+            "(x,x,x,x,x,x,x,x,x)",
+            "(x,x,x,x,x,x,x,x,x)",
+            "(1,x,x,x,x,x,x,x,x))",
+            "(x,x,x,x,x,x,x,x,x)",
+            "(x,x,x,x,x,x,x,x,x)",
+            "(x,x,x,x,x,x,x,x,x)",
+            "(x,x,x,x,x,x,x,x,x)",
+            "(x,x,x,x,x,x,x,x,x)"
+        };
+
         static void Main(string[] args)
         {
             Console.WriteLine(SudokuQuadrantChecker(strArr));
@@ -19,6 +60,8 @@ namespace CoderbyteTest_SudokuQuadrantChecker
 
         public static string SudokuQuadrantChecker(string[] strArr)
         {
+            List<int> subGridIndList = new List<int>(); 
+
             string[,] deskArr = new string[dimX, dimY];
 
             int x = 0;
@@ -35,88 +78,130 @@ namespace CoderbyteTest_SudokuQuadrantChecker
 
             //finding double in the lines
 
-            Dictionary<string, int> dicLine = new Dictionary<string, int>();            
-
             for (int i = 0; i < dimX; i++)
             {
+                Dictionary<string, int> dicLine = new Dictionary<string, int>();
+
                 for (int j = 0; j < dimY; j++)
                 {
                     string dig = deskArr[i, j];
 
                     if (dig != "x")
                     {
-                        if(dicLine.ContainsKey(deskArr[i, j]))                            
+                        if (dicLine.ContainsKey(dig))
                         {
                             dicLine[deskArr[i, j]] += 1;
                         }
-                        else 
+                        else
                         {
-                            dicLine.Add(deskArr[i, j], 1);
+                            dicLine.Add(dig, 1);
                         }
+                    }
+                }
 
-                        foreach (var item in dicLine.Keys)
+                foreach (var item in dicLine.Keys)
+                {
+                    if (dicLine[item] > 1)
+                    {
+                        for (int z = 0; z < dimX; z++)
                         {
-                            if (dicLine[item] >1)
+                            if (deskArr[i, z] == item)
                             {
-                                for (int z = 0; z < dimX; z++)
-                                {
-                                    if(deskArr[i, z] == item)
-                                    {
-                                        GetSubGrid(i, z);
-                                    }
-                                }
+                                int subGridInd = GetSubGridInd(i, z);
+                                subGridIndList.Add(subGridInd);
                             }
-
                         }
-
-
                     }
                 }
             }
 
-                // code goes here  
-                return null;
-        }
+            //finding double in the columns
 
+            for (int i = 0; i < dimY; i++)
+            {
+                Dictionary<string, int> dicLine = new Dictionary<string, int>();
+
+                for (int j = 0; j < dimX; j++)
+                {
+                    string dig = deskArr[j, i];
+
+                    if (dig != "x")
+                    {
+                        if (dicLine.ContainsKey(dig))
+                        {
+                            dicLine[deskArr[j, i]] += 1;
+                        }
+                        else
+                        {
+                            dicLine.Add(dig, 1);
+                        }
+                    }
+                }
+
+                foreach (var item in dicLine.Keys)
+                {
+                    if (dicLine[item] > 1)
+                    {
+                        for (int z = 0; z < dimY; z++)
+                        {
+                            if (deskArr[z, i] == item)
+                            {
+                                int subGridInd = GetSubGridInd(z, i);
+                                subGridIndList.Add(subGridInd);
+                            }
+                        }
+                    }
+                }
+            }
+
+            List<int> subGridIndexes = subGridIndList.Distinct<int>().ToList();
+            
+            subGridIndexes.Sort();
+
+            string subGridIndStr = String.Join(", ", subGridIndexes);
+
+            return subGridIndStr;
+        }
+   
         static int GetSubGridInd(int x, int y)        
         {
             int ind = 0;
 
-            if(x <= 3 && y <= 3)
+            if(x <= 2 && y <= 2)
             {
                 ind = 1;
             }
-            else if (x <= 6 && y <= 3)
+            else if (x <= 2 && y <= 5)
             {
                 ind = 2;
             }
-            else if (x <= 9 && y <= 3)
+            else if (x <= 2 && y <= 8)
             {
                 ind = 3;
             }
 
-            else if (x <= 3 && y <= 6)
+            else if (x <= 5 && y <= 2)
             {
                 ind = 4;
             }
-            else if (x <= 6 && y <= 6)
+            else if (x <= 5 && y <= 5)
             {
                 ind = 5;
             }
-            else if (x <= 9 && y <= 6)
+            else if (x <= 5 && y <= 8)
             {
                 ind = 6;
             }
 
-            else if (x <= 3 && y <= 9)
+            else if (x <= 8 && y <= 2)
             {
                 ind = 7;
             }
-            else if (x <= 6 && y <= 9)
+            else if (x <= 8 && y <= 5)
             {
                 ind = 8;
             }
-            else if (x <= 9 && y <= 9)
+            else if (x <= 8 && y <= 8)
             {
                 ind = 9;
             }
